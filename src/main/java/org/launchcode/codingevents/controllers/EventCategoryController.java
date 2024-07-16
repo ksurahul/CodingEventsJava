2bb0ff15f8a484a372570c57bcd6cab88bf15322
unknown <rahulpksu@gmail.com>
2024-07-15 21:40:44 -0500

    test

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -5,12 +5,12 @@ import org.launchcode.codingevents.data.EventCategoryRepository;
 import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 
 /**
  * Created by Chris Bay
